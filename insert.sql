5dec6f5310aca2529f3128744d1df09f4ce4675f
StepanMelikyan <98053309+StepanMelikyan@users.noreply.github.com>
2023-03-12 22:03:34 +0300

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO `vk`.`users` (`id`, `firstname`, `lastname`, `email`, `password_hash`, `phone`) 
+VALUES 
+('1', 'Davis', 'Davidovich', 'as@mail.ru', '123456', '99999999'),
+('2', 'Saha', 'Aleksandrovich', 'sa@mail.ru', '87654', '9997659'),
+('3', 'David2', 'Davidovich2', 'as2@mail.ru', '123456', '99876999'),
+('4', 'David3', 'Davidovich3', 'as3@mail.ru', '12376', '9999899'),
+('5', 'David4', 'Davidovich5', 'as4@mail.ru', '12986', '991234999'),
+('6', 'Davi5', 'Davidovich5', 'as5@mail.ru', '16546', '9994569'),
+('7', 'Davi6', 'Davidovich6', 'as6@mail.ru', '27756', '999899'),
+('8', 'Davi7', 'Davidovich7', 'as7@mail.ru', '656456', '965456599999'),
+('9', 'Davi8', 'Davidovich8', 'as8@mail.ru', '120956', '987879'),
+('10', 'Davi9', 'Davidovich9', 'as9@mail.ru', '11212456', '979999');
+
+INSERT INTO `vk`.`media_types` (`id`, `name`) 
+VALUES 
+('1', 'cat'),
+('2', 'dog'),
+('3', 'bird'),
+('4', 'me'),
+('5', 'sister'),
+('6', 'home'),
+('7', 'city'),
+('8', 'school'),
+('9', 'car'),
+('10', 'sun');
